55e9bc79d09bc7d3e5ca918aaa9d7313330e87a6
Johan Eskils <eskils@telia.com>
2023-09-03 10:12:01 +0200

    övningsuppgifter

---
+++ 20230901/ex1.py
@@ -0,0 +1,4 @@
+a = input('Vad heter du? ')
+age = int(input('Ålder? '))
+print("\n")
+print('Jag heter ',a,' och är', age, "år gammal.")
---
+++ 20230901/ex2_3.py
@@ -0,0 +1,3 @@
+F = float(input('Grader i Farenheit: '))
+C = float((F-32)*5/9)
+print(f"{F:.1f} grader F motsvarar {C:.1f} grader C")
