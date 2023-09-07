37842ea05af092fbb017c8a441013534ab2ae4c9
JEskilsAIDEV23 <143608346+JEskilsAIDEV23@users.noreply.github.com>
2023-09-07 16:33:23 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-prompt = "C:\\Python_Grund2023\\"
-mapp = input('Vilken mapp finns filen i?')
-filnamn = input('Vad heter text filen?')
-pathen = prompt+mapp+"\\"+filnamn
-with open(pathen, 'r',  encoding='UTF-8') as f:
-    for i in f:
-        print (i)
