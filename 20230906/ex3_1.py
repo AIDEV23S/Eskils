cd3408356f89631dceec67a3a13cd19e14600863
JEskilsAIDEV23 <143608346+JEskilsAIDEV23@users.noreply.github.com>
2023-09-07 16:31:54 +0200

    Delete 20230906 directory

--- 20230906/ex3_1.py
+++
@@ -1,9 +0,0 @@
-minuter = int(input('Hur många minuter ringer du per månad? '))
-if minuter < 33:
-    abonnemang = "Kontant"
-if minuter > 66:
-    abonnemang = "Plus"
-else:
-    abonnemang = "Normal"
-
-print("Du bör välja abonnemanget: ", abonnemang)
--- 20230906/ex4_2.py
+++
@@ -1,3 +0,0 @@
-print('tal','\tkvadrat','\tkubik')
-for i in range(1,13):
-    print(i,'\t'+str(i**2),'\t\t'+str(i**3))
